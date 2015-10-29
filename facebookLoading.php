6060e957119792a66f0ed5f193c9bbba5b1f2cfe
KenAlin <kek.serv@gmail.com>
2015-10-30 00:58:37 +0100

    Production
    * Mise à jour avec l'API Facebook
    * Système de mise en cache (pour éviter de faire appel à l'API tout le
    temps)
    * Ajout de QR codes
    * Réduction du poids global des images
    * Meilleur feeling sur les interfaces tactiles

--- facebookLoading.php
+++ facebookLoading.php
@@ -1,67 +1,88 @@
+<?php
 /*
 * @author Corentin CLOAREC & Jules CORDONNIER
 */
 
-<?php
-// The id of the page L'Agenda
-$fb_page_id = "1436089993356040";
+header('Content-Type: application/json; charset=utf-8');
+setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR.UTF-8');
 
-// The acces token of the application
-$access_token = "367173486810462|Mg9xTK-LHVrNm45ni4O662B9hCw";
-// The acces token of the page (to delete posts)
-$page_access_token = "CAAFN8T1AlV4BAJyocN14itEZCpja1L1FcNhqbeYWlkdxbqwQaB16BYxkYVSHbQPPLjXXun41E8Op4LKtktHZCGxK7OMKmV0FpZCenz8UAwpxHRPv0OZA8EnfGKHAzIXosGetnwIy30QLINNSVjG99AZAomlUJtAqhqE5cWbZBxa2dYAVftsYhDL3JoFA4D35ZCxEkMapRczkGa782ZA3IDWxJVZClv0O85nEELx70LkAX5gZDZD";
+function jsonRemoveUnicodeSequences($struct) {
+   return preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+}
 
-// Get the posts on the page (= tagged)
-$fields="link,to"; 
-$json_link = "https://graph.facebook.com/{$fb_page_id}/tagged/?fields={$fields}&access_token={$access_token}&limit=50";
-$json = file_get_contents($json_link);
-$obj = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
+if (time() - filemtime('cache/update.token') > 120) {
+  // Ne recharge le cache qu'une fois à intervalles régulier - attente d'au moins 2min entre chaque recharge - automatique toutes les heures (tache cron)
+  touch('cache/update.token');
+  // The id of the page L'Agenda
+  $fb_page_id = "1436089993356040";
 
-// Hide warning errors
-error_reporting(E_ERROR | E_PARSE);
+  // The acces token of the application
+  $access_token = "367173486810462|Mg9xTK-LHVrNm45ni4O662B9hCw";
+  // The acces token of the page (to delete posts)
+  $page_access_token = "CAAFN8T1AlV4BAJyocN14itEZCpja1L1FcNhqbeYWlkdxbqwQaB16BYxkYVSHbQPPLjXXun41E8Op4LKtktHZCGxK7OMKmV0FpZCenz8UAwpxHRPv0OZA8EnfGKHAzIXosGetnwIy30QLINNSVjG99AZAomlUJtAqhqE5cWbZBxa2dYAVftsYhDL3JoFA4D35ZCxEkMapRczkGa782ZA3IDWxJVZClv0O85nEELx70LkAX5gZDZD";
 
- // count the number of posts
-$post_count = count($obj['data']);
-$eventsArray = array();
-    for($i=0; $i<$post_count; $i++){
-        //look if the link in the post is an event
-        $eventLink = $obj['data'][$i]['link'];
-        if (preg_match("#https://www.facebook.com/events/[0-9]*#", $eventLink)){
-            if (isset($obj['data'][$i]['to']['data'][1])) { // only existing
-	            // get the event's ID
-	            $eventId = $obj['data'][$i]['to']['data'][1]['id'];
-	            // look if the event is not already added
-	            if(!(in_array($eventId, $eventsArray))){
-	                // get the event's start time
-	                $date = $obj['data'][$i]['to']['data'][1]['start_time'];
-	                //We look if the event is past or not
-	                $timestamp = strtotime($date);
-	                if($timestamp > time()){
-	                    //we add the event to the list
-	                    $eventsArray[$date] = $eventId;
-	                }
-	                else {
-	                	//delete past posts
-			 			$postToDelete = $obj['data'][$i]['id'];
-			    		$json_link2 = "https://graph.facebook.com/{$postToDelete}/?access_token={$page_access_token}&method=delete";
-			    		$json2 = file_get_contents($json_link2);
-        			}
-	            }
-        	}
-        	else {
-        		//delete fake event link post
-	 			$postToDelete = $obj['data'][$i]['id'];
-	    		$json_link2 = "https://graph.facebook.com/{$postToDelete}/?access_token={$page_access_token}&method=delete";
-	    		$json2 = file_get_contents($json_link2);
-        	}	
-        }
-        else {
-        	//delete not event post
- 			$postToDelete = $obj['data'][$i]['id'];
-    		$json_link2 = "https://graph.facebook.com/{$postToDelete}/?access_token={$page_access_token}&method=delete";
-    		$json2 = file_get_contents($json_link2);
-        }
-    }
-// sort the array by date (= by key)
-ksort($eventsArray);
+  // Get the posts on the page (= tagged)
+  $fields="link,to,description,message";
+  $json_link = "https://graph.facebook.com/{$fb_page_id}/feed/?fields={$fields}&access_token={$access_token}&limit=100";
+  $json = file_get_contents($json_link);
+  $obj = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
+
+  // Hide warning errors
+  error_reporting(E_ERROR | E_PARSE);
+
+   // count the number of posts
+  $post_count = count($obj['data']);
+  $eventsArray = array();
+      for($i=0; $i<$post_count; $i++){
+          //look if the link in the post is an event
+          $eventLink = $obj['data'][$i]['message'];
+          if (preg_match("#https://www.facebook.com/events/([0-9]+)/#", $eventLink)){
+              if (isset($obj['data'][$i]['to']['data'][1])) { // only existing
+  	            // get the event's ID
+  	            $eventId = $obj['data'][$i]['to']['data'][1]['id'];
+  	            // look if the event is not already added
+  	            if(!(in_array($eventId, $eventsArray))){
+  	                // get the event's start time
+  	                $date = $obj['data'][$i]['to']['data'][1]['start_time'];
+  	                //We look if the event is past or not
+  	                $timestamp = strtotime($date);
+                  $eventsForces = array(/* "757693680994459", "924459167593710", "788978327818430", "887044594680093" */);
+                    $eventsBannis = array("972299266145646");
+  	                if( !in_array($eventId, $eventsBannis) && ($timestamp > time() || in_array($eventId, $eventsForces)) ){
+  	                    //we add the event to the list
+  	                    $eventsArray[$date] = $eventId;
+  	                }
+  	                else {
+  	                	//delete past posts
+  			 			$postToDelete = $obj['data'][$i]['id'];
+  			    		$json_link2 = "https://graph.facebook.com/{$postToDelete}/?access_token={$page_access_token}&method=delete";
+  			    		$json2 = file_get_contents($json_link2);
+          			}
+  	            }
+          	}
+          	else {
+          		//delete fake event link post
+  	 			$postToDelete = $obj['data'][$i]['id'];
+  	    		$json_link2 = "https://graph.facebook.com/{$postToDelete}/?access_token={$page_access_token}&method=delete";
+  	    		$json2 = file_get_contents($json_link2);
+          	}
+          }
+          else {
+          	//delete not event post
+   			$postToDelete = $obj['data'][$i]['id'];
+      		$json_link2 = "https://graph.facebook.com/{$postToDelete}/?access_token={$page_access_token}&method=delete";
+      		$json2 = file_get_contents($json_link2);
+          }
+      }
+  // sort the array by date (= by key)
+  ksort($eventsArray);
+
+  // Met les évènements en cache
+  $file = "cache/events.json";
+  $current = jsonRemoveUnicodeSequences($eventsArray);
+  file_put_contents($file, $current);
+}
+
+
+header('Location: .');
 ?>
--- index.php
+++ index.php
@@ -1,25 +1,25 @@
-/*
+<?php /*
 * @author Corentin CLOAREC & Jules CORDONNIER
 */
 
-<?php include 'facebookLoading.php'; ?>
+// The acces token of the application
+$access_token = "367173486810462|Mg9xTK-LHVrNm45ni4O662B9hCw";
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
- 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
- 
+
     <title>L'Agenda Montpellier</title>
- 
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="css/style.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.0/isotope.pkgd.min.js"></script>
-    <script src="js/jquery.fittext.js"></script>    
+    <script src="js/jquery.fittext.js"></script>
     <script src="js/imagesloaded.pkgd.min.js"></script>
     <script src="js/jquery.kinetic.js"></script>
 
@@ -49,31 +49,31 @@
                 Comment publier un événement ?
             </div>
             <div class="contenu">
-                <p><b>Cest simple...</b></p>
+                <p><b>C'est simple...</b></p>
                 <ul>
-                    <li>Copiez le lien dun évènement Facebook <b>public</b>.</li>
-                    <li>Publiez le lien sur la page Facebook de lAgenda.</li>                
+                    <li>Copiez le lien d'un événement Facebook <b>public</b></li>
+                    <li>Publiez le lien sur la page Facebook de l'Agenda</li>
                 </ul>
                 <p class="lien"><b>facebook.com/agendamontpellier</b></p>
-                <img src='http://chart.apis.google.com/chart?cht=qr&amp;chs=200x200&amp;chl=https://www.facebook.com/agendamontpellier' id='facebookPage'>
-                <p class="refresh">Rechargez la page !<p>
+                <div id="QRCodeFB"></div>
+                <p class="refresh">Puis <a href="facebookLoading">cliquez ici</a><p>
             </div>
-        <h1>L'Agenda</h1>
+        <h1>L'Agenda étudiant Montpellier</h1>
         <img src="images/refresh.png" id="refreshImage">
-        <p class="slogan">Trouvez et partagez des évènements à Montpellier !</p>
+        <p class="slogan">Trouvez et partagez des événements à Montpellier !</p>
     </div>
 
 <div id="container"></div>
 
-<div class="loadmore"> Chargement en cours... </div>
+<div class="loadmore">Chargement en cours...</div>
 
 <script type="text/javascript">
-    // pass the PHP variables to js (events and access token)
-    var events = <?php echo json_encode($eventsArray); ?>;
+    // On laisse la magie opérer (les évènements sont en cache, pour plus de rapidité au chargement)
+    var events = <?php echo file_get_contents("cache/events.json"); ?>;
     var access_token = <?php echo json_encode($access_token); ?>;
 </script>
 
-<script src="js/main.js"></script> 
+<script src="js/main.js"></script>
 
 </body>
 </html>
